4c65e5e6b5749eaf73de2400c5126110c60b2562
didi <didi@192.168.1.7>
2020-12-02 11:01:58 +0800

    Download and preprocess the datasets.

---
+++ datasets/tods_datasets.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+
+from tods_dataset_base import TODS_dataset
+
+class kpi_dataset(TODS_dataset):
+    resources = [
+        # ("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
+        # ("https://github.com/datamllab/tods/blob/master/datasets/anomaly/kpi/TRAIN/dataset_TRAIN/tables/learningData.csv", None),
+        # ("https://github.com/NetManAIOps/KPI-Anomaly-Detection/blob/master/Preliminary_dataset/train.csv", None),
+        ("https://hegsns.github.io/tods_datasets/kpi/TRAIN/dataset_TRAIN/tables/learningData.csv", None), # it needs md5 to check if local learningData.csv is the same with online.
+        # needs a server to store the dataset.
+        # ("https://raw.githubusercontent.com/datamllab/tods/master/datasets/anomaly/kpi/TRAIN/dataset_TRAIN/tables/learningData.csv", None), # it needs md5 to check if local learningData.csv is the same with online.
+    ]
+
+    training_file = 'learningData.csv'
+    testing_file = 'testingData.csv'
+    ground_truth_index = 3
+    _repr_indent = 4
+
+    # def __init__(self, root, train, transform=None, target_transform=None, download=True):
+    #     super().__init__(root, train, transform=None, target_transform=None, download=True)
+
+    def process(self) -> None:
+
+        print('Processing...')
+
+        training_set_fname = os.path.join(self.raw_folder, 'learningData.csv')
+        self.training_set_dataframe = pd.read_csv(training_set_fname)
+        testing_set_fname = os.path.join(self.raw_folder, 'learningData.csv')  # temperarily same with training set
+        self.testing_set_dataframe = pd.read_csv(testing_set_fname)
+
+        self.process_dataframe()
+        self.training_set_dataframe.to_csv(os.path.join(self.processed_folder, self.training_file))
+        self.testing_set_dataframe.to_csv(os.path.join(self.processed_folder, self.testing_file))
+
+        print('Done!')
+
+
+class yahoo_dataset(TODS_dataset):
+    resources = [
+        # ("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
+        # ("https://github.com/datamllab/tods/blob/master/datasets/anomaly/kpi/TRAIN/dataset_TRAIN/tables/learningData.csv", None),
+        # ("https://github.com/NetManAIOps/KPI-Anomaly-Detection/blob/master/Preliminary_dataset/train.csv", None),
+        ("https://hegsns.github.io/tods_datasets/yahoo_sub_5/TRAIN/dataset_TRAIN/tables/learningData.csv", None), # it needs md5 to check if local learningData.csv is the same with online.
+        # needs a server to store the dataset.
+        # ("https://raw.githubusercontent.com/datamllab/tods/master/datasets/anomaly/kpi/TRAIN/dataset_TRAIN/tables/learningData.csv", None), # it needs md5 to check if local learningData.csv is the same with online.
+    ]
+
+    training_file = 'learningData.csv'
+    testing_file = 'testingData.csv'
+    ground_truth_index = 3
+    _repr_indent = 4
+
+    # def __init__(self, root, train, transform=None, target_transform=None, download=True):
+    #     super().__init__(root, train, transform=None, target_transform=None, download=True)
+
+    def process(self) -> None:
+
+        print('Processing...')
+
+        training_set_fname = os.path.join(self.raw_folder, 'learningData.csv')
+        self.training_set_dataframe = pd.read_csv(training_set_fname)
+        testing_set_fname = os.path.join(self.raw_folder, 'learningData.csv')  # temperarily same with training set
+        self.testing_set_dataframe = pd.read_csv(testing_set_fname)
+
+        self.process_dataframe()
+        self.training_set_dataframe.to_csv(os.path.join(self.processed_folder, self.training_file))
+        self.testing_set_dataframe.to_csv(os.path.join(self.processed_folder, self.testing_file))
+
+        print('Done!')
+
+
+
+
+# kpi_dataset(root='./datasets', train=True, transform='binarize')
+# yahoo_dataset(root='./datasets', train=True, transform='binarize')
--- examples/build_IsolationForest_pipline.py
+++ examples/build_IsolationForest_pipline.py
@@ -15,80 +15,80 @@ step_0.add_output('produce')
 pipeline_description.add_step(step_0)
 
 # # Step 1: column_parser
-primitive_1 = index.get_primitive('d3m.primitives.data_transformation.column_parser.Common')
+primitive_1 = index.get_primitive('d3m.primitives.tods.data_processing.column_parser')
 step_1 = PrimitiveStep(primitive=primitive_1)
 step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
 step_1.add_output('produce')
 pipeline_description.add_step(step_1)
 
 # Step 2: extract_columns_by_semantic_types(attributes)
-step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.Common'))
+step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.extract_columns_by_semantic_types'))
 step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_2.add_output('produce')
 step_2.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE, data=['https://metadata.datadrivendiscovery.org/types/Attribute'])
 pipeline_description.add_step(step_2)
 
 # Step 3: extract_columns_by_semantic_types(targets)
-step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.extract_columns_by_semantic_types.Common'))
-step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
-step_3.add_output('produce')
-step_3.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
-							data=['https://metadata.datadrivendiscovery.org/types/TrueTarget'])
-pipeline_description.add_step(step_3)
+# step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.extract_columns_by_semantic_types'))
+# step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+# step_3.add_output('produce')
+# step_3.add_hyperparameter(name='semantic_types', argument_type=ArgumentType.VALUE,
+# 							data=['https://metadata.datadrivendiscovery.org/types/TrueTarget'])
+# pipeline_description.add_step(step_3)
 
 attributes = 'steps.2.produce'
-targets = 'steps.3.produce'
+# targets = 'steps.3.produce'
 
-# Step 4: Power transformation 
-primitive_4 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.power_transformer')
-step_4 = PrimitiveStep(primitive=primitive_4)
-step_4.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
-step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=attributes)
-step_4.add_output('produce')
-pipeline_description.add_step(step_4)
-
-# Step 5: Axiswise scaling 
-primitive_5 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.axiswise_scaler')
-step_5 = PrimitiveStep(primitive=primitive_5)
-step_5.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
-step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-step_5.add_output('produce')
-pipeline_description.add_step(step_5)
-
-# Step 6: Standarization 
-primitive_6 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.standard_scaler')
-step_6 = PrimitiveStep(primitive=primitive_6)
-step_6.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
-step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
-step_6.add_output('produce')
-pipeline_description.add_step(step_6)
-
-# Step 7: Quantile transformation 
-primitive_7 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.quantile_transformer')
-step_7 = PrimitiveStep(primitive=primitive_7)
-step_7.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
-step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
-step_7.add_output('produce')
-pipeline_description.add_step(step_7)
+# # Step 4: Power transformation
+# primitive_4 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.power_transformer')
+# step_4 = PrimitiveStep(primitive=primitive_4)
+# step_4.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
+# step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=attributes)
+# step_4.add_output('produce')
+# pipeline_description.add_step(step_4)
+#
+# # Step 5: Axiswise scaling
+# primitive_5 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.axiswise_scaler')
+# step_5 = PrimitiveStep(primitive=primitive_5)
+# step_5.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
+# step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+# step_5.add_output('produce')
+# pipeline_description.add_step(step_5)
+#
+# # Step 6: Standarization
+# primitive_6 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.standard_scaler')
+# step_6 = PrimitiveStep(primitive=primitive_6)
+# step_6.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
+# step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+# step_6.add_output('produce')
+# pipeline_description.add_step(step_6)
+#
+# # Step 7: Quantile transformation
+# primitive_7 = index.get_primitive('d3m.primitives.tods.timeseries_processing.transformation.quantile_transformer')
+# step_7 = PrimitiveStep(primitive=primitive_7)
+# step_7.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='new')
+# step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+# step_7.add_output('produce')
+# pipeline_description.add_step(step_7)
 
 # Step 4: Isolation Forest 
 primitive_8 = index.get_primitive('d3m.primitives.tods.detection_algorithm.pyod_iforest')
 step_8 = PrimitiveStep(primitive=primitive_8)
 step_8.add_hyperparameter(name='contamination', argument_type=ArgumentType.VALUE, data=0.1)
-step_8.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.7.produce')
+step_8.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference=attributes) # 'steps.7.produce')
 # step_8.add_output('produce_score')
 step_8.add_output('produce')
 pipeline_description.add_step(step_8)
 
 # Step 5: Predictions
-step_9 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.Common'))
-step_9.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.8.produce')
+step_9 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.tods.data_processing.construct_predictions'))
+step_9.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
 step_9.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
 step_9.add_output('produce')
 pipeline_description.add_step(step_9)
 
 # Final Output
-pipeline_description.add_output(name='output predictions', data_reference='steps.9.produce')
+pipeline_description.add_output(name='output predictions', data_reference='steps.4.produce')
 
 # Output to json
 data = pipeline_description.to_json()
